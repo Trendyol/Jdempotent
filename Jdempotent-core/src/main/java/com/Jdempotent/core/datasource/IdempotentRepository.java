0e982410acfbc4a668a5b2e7fefbcaf2d65ee89b
Mehmet <mehmet.ari@trendyol.com>
2020-10-11 16:23:20 +0300

    added provious readme file need to update..

--- Jdempotent-core/src/main/java/com/Jdempotent/core/datasource/IdempotentRepository.java
+++ Jdempotent-core/src/main/java/com/Jdempotent/core/datasource/IdempotentRepository.java
@@ -5,22 +5,16 @@ import com.Jdempotent.core.model.IdempotentRequestWrapper;
 import com.Jdempotent.core.model.IdempotentResponseWrapper;
 
 /**
- *
  * an interface that the functionality required of a request store for idempotent method invocations.
- *
-
- *
  */
 public interface IdempotentRepository {
     /**
-     *
      * @param key
      * @return
      */
     boolean contains(IdempotencyKey key);
 
     /**
-     *
      * Checks the cache for an existing call for this request
      *
      * @param key
@@ -29,20 +23,17 @@ public interface IdempotentRepository {
     IdempotentResponseWrapper getResponse(IdempotencyKey key);
 
     /**
-     *
      * @param key
      * @param requestObject
      */
     void store(IdempotencyKey key, IdempotentRequestWrapper requestObject);
 
     /**
-     *
      * @param key
      */
     void remove(IdempotencyKey key);
 
     /**
-     *
      * @param request
      * @param idempotentResponse
      */
