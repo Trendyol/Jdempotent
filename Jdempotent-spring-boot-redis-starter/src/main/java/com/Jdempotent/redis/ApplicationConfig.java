3b229e618b384fda097ca2c0fd922d9da7a6bbb7
Mehmet <mehmet.ari@trendyol.com>
2020-10-12 12:21:45 +0300

    added enable config and gitiognore file

--- Jdempotent-spring-boot-redis-starter/src/main/java/com/Jdempotent/redis/ApplicationConfig.java
+++ Jdempotent-spring-boot-redis-starter/src/main/java/com/Jdempotent/redis/ApplicationConfig.java
@@ -3,19 +3,27 @@ package com.Jdempotent.redis;
 
 import com.Jdempotent.core.aspect.IdempotentAspect;
 import com.Jdempotent.core.callback.ErrorConditionalCallback;
-import com.Jdempotent.redis.RedisIdempotentRepository;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.RedisTemplate;
 
 /**
- * 
+ *
  */
 @Configuration
+@ConditionalOnProperty(
+        value = "jdempotent.enable",
+        havingValue = "true",
+        matchIfMissing = true)
 public class ApplicationConfig {
 
     @Bean
+    @ConditionalOnProperty(
+            value = "jdempotent.enable",
+            havingValue = "true",
+            matchIfMissing = true)
     @ConditionalOnClass(ErrorConditionalCallback.class)
     public IdempotentAspect getLogMethodExecutionTimeAspect(RedisTemplate redisTemplate, ErrorConditionalCallback errorConditionalCallback) {
         return new IdempotentAspect(new RedisIdempotentRepository(redisTemplate), errorConditionalCallback);
