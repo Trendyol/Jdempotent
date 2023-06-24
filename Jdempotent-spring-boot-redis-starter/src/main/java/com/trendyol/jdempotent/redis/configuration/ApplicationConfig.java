2c267693094e184b9340b03419b57db5e2e81c35
Samuel Cunha <samukasaww@live.com>
2023-06-24 11:02:19 -0300

    Adjustment in the creation of Beans

--- Jdempotent-spring-boot-redis-starter/src/main/java/com/trendyol/jdempotent/redis/configuration/ApplicationConfig.java
+++ Jdempotent-spring-boot-redis-starter/src/main/java/com/trendyol/jdempotent/redis/configuration/ApplicationConfig.java
@@ -5,7 +5,9 @@ import com.trendyol.jdempotent.core.aspect.IdempotentAspect;
 import com.trendyol.jdempotent.core.callback.ErrorConditionalCallback;
 import com.trendyol.jdempotent.redis.repository.RedisIdempotentRepository;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -32,8 +34,8 @@ public class ApplicationConfig {
             prefix="jdempotent", name = "enable",
             havingValue = "true",
             matchIfMissing = true)
-    @ConditionalOnClass(ErrorConditionalCallback.class)
-    public IdempotentAspect getIdempotentAspect(@Qualifier("JdempotentRedisTemplate") RedisTemplate redisTemplate, ErrorConditionalCallback errorConditionalCallback) {
+    @ConditionalOnBean(ErrorConditionalCallback.class)
+    public IdempotentAspect getIdempotentAspectOnErrorConditionalCallback(@Qualifier("JdempotentRedisTemplate") RedisTemplate redisTemplate, ErrorConditionalCallback errorConditionalCallback) {
         return new IdempotentAspect(new RedisIdempotentRepository(redisTemplate, redisProperties), errorConditionalCallback);
     }
 
